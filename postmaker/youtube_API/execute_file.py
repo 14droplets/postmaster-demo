688a76371cc208b891c1a5617a7b35e49f4f1f2e
Vert1g0 <editingtimeap@gmail.com>
2021-09-29 16:24:46 +0300

    Добавил Ютуб

---
+++ postmaker/youtube_API/execute_file.py
@@ -0,0 +1,2 @@
+
+# py upload_video.py --file="/PYTHON/TEST.avi" --title="Велозаезд_тест" --description="Покатушка миллион лет назад" --keywords="велосипед, лето, дорога" --category="22" --privacyStatus="public"
